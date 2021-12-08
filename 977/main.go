4d78e51f7f0fca15de0ca08678a876a0e9af7767
liyouyou <uu@querycap.com>
2021-12-08 10:10:20 +0800

    977: 有序数组的平方

--- 977/main.go
+++ 977/main.go
@@ -1,29 +1,32 @@
 package main
 
+import (
+	"sort"
+)
+
 func sortedSquares(nums []int) []int {
-	if len(nums) < 1 {
-		return []int{}
+	rNums := make([]int, len(nums))
+	for i,v := range nums {
+		rNums[i] = v*v
 	}
-	result := []int{nums[0] * nums[0]}
-	for i := 1; i < len(nums); i++ {
-		if nums[i] < 0 {
-			result = append([]int{nums[i] * nums[i]}, result...)
-		} else {
-			if nums[i]*nums[i] >= result[i-1] {
-				result = append(result, nums[i]*nums[i])
-			} else if nums[i]*nums[i] <= result[0] {
-				result = append([]int{nums[i] * nums[i]}, result...)
-			} else {
-				lenResult := len(result)
-				for j := 0; j < len(result); j++ {
-					if result[j] >= nums[i]*nums[i] {
-						result = append(result[0:j], nums[i]*nums[i])
-						result = append(result, result[j+1:lenResult]...)
-						continue
-					}
-				}
-			}
+
+	sort.Ints(rNums)
+	return rNums
+}
+
+// 双指针
+func sortedSquares2(nums []int) []int {
+	rNums := make([]int, len(nums))
+	i,j := 0, len(nums) -1
+	for pos:=j;pos>=0;pos--{
+		if nums[i]*nums[i] > nums[j]*nums[j] {
+			rNums[pos] = nums[i]*nums[i]
+			i++
+		}else{
+			rNums[pos] = nums[j]*nums[j]
+			j--			
 		}
 	}
-	return result
+
+	return rNums
 }
